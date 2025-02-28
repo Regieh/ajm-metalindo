bd24364d8ba167756a2f01e670aaf1149734db6f
FKhorasani <fernando.khorasani@gmail.com>
2025-02-28 16:43:09 +0700

    Update Projects Data, Fix Bug, Update Contact, About to Nav

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,6 +1,7 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
+  { name: "About Us", url: "/about" },
   { name: "Projects", url: "/projects" },
   { name: "Services", url: "/services" },
   // { name: "Blog", url: "/blog" },
