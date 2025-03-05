bc800b8140d46833628a75356308d8cd66290222
FKhorasani <fernando.khorasani@gmail.com>
2025-03-05 17:35:55 +0700

    Fix service red bg

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -45,6 +45,11 @@ export default {
       animation: {
         subtleMove: 'subtleMove 6s ease-in-out infinite',
       },
+      screens: {
+        "3xl": "1920px",
+        "4xl": "2560px",
+        "5xl": "3840px",
+      },
     },
   },
   plugins: [
